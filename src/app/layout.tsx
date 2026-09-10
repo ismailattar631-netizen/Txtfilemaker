7a36c10a0663cbc648e4e040377033a02337735f
Ismail Attar <ismailattar631@gmail.com>
2026-09-10 20:43:45 +0530

    Restore layout.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 ﻿import type { Metadata } from 'next';
 import './globals.css';
 import Navbar from '@/components/layout/Navbar';
-import Script from 'next/script'
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Footer from '@/components/layout/Footer';
 import { WebAppJsonLd, HowToJsonLd } from '@/components/seo/JsonLd';
@@ -97,13 +96,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-<Script
-  async
-  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4832541917740546"
-  crossOrigin="anonymous"
-  strategy="afterInteractive"
-/>
-        <script
           dangerouslySetInnerHTML={{
             __html: `(function() {
               try {
